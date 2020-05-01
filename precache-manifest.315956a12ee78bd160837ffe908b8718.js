7fc9a44ec2ffcf322a67209adba829e186fda94d
oneight2 <1606109@sttgarut.ac.id>
2020-05-01 22:20:00 +0700

    Updates

---
+++ precache-manifest.315956a12ee78bd160837ffe908b8718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6725e68449e799325823c9b3c12bb61",
+    "url": "/Belajar-API-React/index.html"
+  },
+  {
+    "revision": "b4352dc90f196f08f619",
+    "url": "/Belajar-API-React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4e6d2a801a6406106975",
+    "url": "/Belajar-API-React/static/js/2.87535394.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Belajar-API-React/static/js/2.87535394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4352dc90f196f08f619",
+    "url": "/Belajar-API-React/static/js/main.c4299143.chunk.js"
+  },
+  {
+    "revision": "3a402aa57fbc76939f6d",
+    "url": "/Belajar-API-React/static/js/runtime-main.c1bda621.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Belajar-API-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
